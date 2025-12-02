4f4ed83346b98e331b46567057525dfa04270656
Evelyn <evelynlivant@gmail.com>
2025-12-02 10:44:32 -0500

    Create CreateTableStudentClass.sql
    Jump table between students and classes

---
+++ CreateTableStudentClass.sql
@@ -0,0 +1,22 @@
+-- Create table StudentClass
+USE [Denormalization]
+GO
+
+/****** Object:  Table [dbo].[StudentClass]    Script Date: 12/2/2025 10:43:13 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[StudentClass](
+	[StudentID] [int] NOT NULL,
+	[ClassID] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_StudentClasse] PRIMARY KEY CLUSTERED 
+(
+	[StudentID] ASC,
+	[ClassID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
