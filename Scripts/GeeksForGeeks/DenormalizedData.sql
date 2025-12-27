eae43c172ee0edd369dce288e59026892858cd37
Evelyn <evelynlivant@gmail.com>
2025-12-27 17:23:35 -0500

    Update DenormalizedData.sql

--- Scripts/GeeksForGeeks/DenormalizedData.sql
+++ Scripts/GeeksForGeeks/DenormalizedData.sql
@@ -1,3 +1,6 @@
+-- https://www.geeksforgeeks.org/dbms/denormalization-in-databases/
+-- Initial data is from Geeks For Geeks.  I added additional rows for more robust data set for practice.
+
 USE [Denormalization]
 GO
 
